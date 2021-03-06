cb397f2266f0546772c90f015629ff811478a2aa
CinisArboris <eyver.evm@gmail.com>
2021-03-06 09:46:59 -0400

    fix : Builder [finalizado/comprobado]

--- Builder/app/src/main/java/com/example/builder/Cliente/Nav.java
+++ Builder/app/src/main/java/com/example/builder/Cliente/Nav.java
@@ -3,6 +3,7 @@ package com.example.builder.Cliente;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.builder.Componentes.Nombre;
 import com.example.builder.Constructores.BuilderM200;
 import com.example.builder.Constructores.BuilderM300;
 import com.example.builder.Director.Armero;
@@ -19,11 +20,18 @@ public class Nav extends AppCompatActivity {
 
         Armero armero = new Armero();
 
+        /*C O N S T R U C C I O N*/
         BuilderM200 builderM200 = new BuilderM200();
         armero.construirTACTICAL(builderM200);
 
         BuilderM300 builderM300 = new BuilderM300();
         armero.contruirINTERVENTION(builderM300);
+
+        /*C O M P O N E N T E S*/
+        builderM200.setNombre(Nombre.M200_INTERVENTION);
+
+        /*C O M P O N E N T E S*/
+        builderM300.setNombre(Nombre.M300_TACTICAL);
 
         /*E X T R A C C I O N*/
         M200 m200 = builderM200.getProducto();
