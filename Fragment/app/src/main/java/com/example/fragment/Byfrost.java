b498e03ba81f05942fe5763a7e83ffa2e4f395dc
CinisArboris <eyver.evm@gmail.com>
2021-03-06 10:08:03 -0400

    fix : Fragment [finalizado/comprobado]

--- Fragment/app/src/main/java/com/example/fragment/Byfrost.java
+++ Fragment/app/src/main/java/com/example/fragment/Byfrost.java
@@ -1,5 +1,10 @@
 package com.example.fragment;
 
 public interface Byfrost {
+
+    /**
+     * Elemento que une los elementos
+     * @param cadena
+     */
     void interface_portal(String cadena);
 }
