ff73a6182b8e7789143e959dbb5cf1b298bfa9d2
CinisArboris <eyver.evm@gmail.com>
2021-04-17 19:00:55 -0400

    Implementando RecyclerView : Avance

--- espresso/app/src/main/java/com/example/espresso/Datos/Persona.java
+++ espresso/app/src/main/java/com/example/espresso/Datos/Persona.java
@@ -1,4 +1,19 @@
 package com.example.espresso.Datos;
 
 public class Persona {
+    int persoan_numero;
+    String persona_nombre;
+
+    public Persona(int persoan_numero, String persona_nombre) {
+        this.persoan_numero = persoan_numero;
+        this.persona_nombre = persona_nombre;
+    }
+
+    public int getPersoan_numero() { return persoan_numero; }
+    public void setPersoan_numero(int persoan_numero) { this.persoan_numero = persoan_numero; }
+    public String getPersona_nombre() { return persona_nombre; }
+    public void setPersona_nombre(String persona_nombre) { this.persona_nombre = persona_nombre; }
+
+    @Override
+    public String toString() { return persona_nombre; }
 }
